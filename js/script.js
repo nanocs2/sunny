809fa53496225a33aeb0052ac081bc997d0b7a50
nanocs2 <nano2010csru@mail.ru>
2023-12-06 18:54:47 +0300

    адаптив

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ const menu = document.getElementById('mobile-menu');
 
 burger.addEventListener("touchstart", () => {
     menu.classList.toggle('mobile-menu--active');
+    burger.classList.toggle('menu-burger__nav--active');
+
 })
 
 
